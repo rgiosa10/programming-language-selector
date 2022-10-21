af60d3074a5c6027842ac53aebe9a3dbc8466688
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-21 09:36:50 -0700

    WIP: adding business logic for calculation of language recommendation

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,64 @@
 // Business logic
 
 function rain (rainSelection) {
+  let totalPoints;
   if (rainSelection === "Yes") {
-    totalPoints += 1;
+    totalPoints = 1;
   } else {
-    totalPoints += 0;
+    totalPoints = 0;
   }
+  return totalPoints;
 };
 
 function vacation (vacationSelection) {
+  let totalPoints;
   if (vacationSelection === "Warm") {
-    totalPoints += 1;
+    totalPoints = 1;
   } else {
-    totalPoints += 0;
+    totalPoints = 0;
   }
+  return totalPoints;
 };
 
 function dogOrCat (dogOrCatSelection) {
+  let totalPoints;
   if (dogOrCat === "Dog") {
-    totalPoints += 1;
+    totalPoints = 1;
   } else {
-    totalPoints += 0;
+    totalPoints = 0;
   }
+  return totalPoints;
 };
 
 function sports (sportsSelection) {
+  let totalPoints;
   if (sportsSelection === "Yes") {
-    totalPoints += 1;
+    totalPoints = 1;
   } else {
-    totalPoints += 0;
+    totalPoints = 0;
   }
+  return totalPoints;
 };
 
 function beachOrHiking (beachOrHikingSelection) {
+  let totalPoints;
   if (beachOrHikingSelection === "Beach") {
-    totalPoints += 1;
+    totalPoints = 1;
   } else {
-    totalPoints += 0;
+    totalPoints = 0;
   }
+  return totalPoints;
 };
 
-function pickLang(totalPoints) {
+function pickLang(rainSelection,vacationSelection,dogOrCatSelection,sportsSelection,beachOrHikingSelection) {
+  let totalPoints = 0;
+
+  totalPoints += rain (rainSelection);
+  totalPoints += vacation (vacationSelection);
+  totalPoints += dogOrCat (dogOrCatSelection);
+  totalPoints += sports (sportsSelection);
+  totalPoints += beachOrHiking (beachOrHikingSelection);
+  
   if (totalPoints === 5) {
     langRecommendation = "Python";
   } else if ( totalPoints < 5 && totalPoints > 1) {
@@ -51,6 +69,9 @@ function pickLang(totalPoints) {
   return langRecommendation;
 };
 
+function addPoints () {
+
+}
 
 function getInputsAndCalc() {
   // get inputs
@@ -60,31 +81,20 @@ function getInputsAndCalc() {
   const sportsSelection = document.querySelector("input[name='sports']:checked").value;
   const beachOrHikingSelection = document.querySelector("input[name='beachOrHiking']:checked").value;
 
-  let totalPoints = 0;
-
-  rain (rainSelection);
-  vacation (vacationSelection);
-  dogOrCat (dogOrCatSelection);
-  sports (sportsSelection);
-  beachOrHiking (beachOrHikingSelection);
-
-  pickLang(totalPoints);
-
-
-  // calculate recommended language
-
-  
-
+  pickLang(rainSelection,vacationSelection,dogOrCatSelection,sportsSelection,beachOrHikingSelection);
 }
 
 // User Interface logic
 
 
-function handleRadio(event) {
-  event.preventDefault();
-  const radioSelection = document.querySelector("input[name='flavor']:checked").value;
-}
-
 window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+  let form = document.querySelector("form");
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    form.addEventListener("submit", getInputsAndCalc);
+    // document.getElementById("radio-form").addEventListener("submit", getInputsAndCalc);
+    document.getElementById("result").removeAttribute("class");
+    document.getElementById("result").innerText = "You should learn to program in " + pickLang();
+  })
 });
