6cffb17c387a89ae4ef4f5fcc74a3e6f5ef82eab
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-21 15:34:40 -0700

    BUGFIX: corrected issue where reset button styling was not reflecting correctly

--- js/scripts.js
+++ js/scripts.js
@@ -79,11 +79,11 @@ function getInputsAndCalc() {
 function langSummary() {
   let langSummary;
   if (langRecommendation === "Python") {
-    langSummary = "About: Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. It is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming. It is often described as a 'batteries included' language due to its comprehensive standard library.";
+    langSummary = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. It is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming. It is often described as a 'batteries included' language due to its comprehensive standard library.";
   } else if (langRecommendation === "JavaScript") {
-    langSummary = "About: JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.";
+    langSummary = "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.";
   } else {
-    langSummary = "About: Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency. It is often referred to as Golang because of its former domain name, golang.org, but its proper name is Go.";
+    langSummary = "Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency. It is often referred to as Golang because of its former domain name, golang.org, but its proper name is Go.";
   }
   return langSummary;
 }
@@ -94,8 +94,9 @@ function langSummary() {
 function reset() {
   // Unhide form submit button
   document.getElementById("form-submit-button").setAttribute("class", "btn btn-primary btn-lg");
-  // Hide the results section that includes reset button
+  // Hide the results section and reset button
   document.getElementById("result").setAttribute("class", "hidden");
+  document.getElementById("resetBtn").setAttribute("class", "hidden");
 
   // Reset form inputs to default values
   document.getElementById("rainDefault").checked = true;
@@ -123,12 +124,12 @@ window.addEventListener("load", function() {
     document.getElementById("form-submit-button").setAttribute("class", "hidden");
 
     // Update results messaging with recommendation from survey
-    document.getElementById("resultText").innerText = "You should learn to program " + langRecommendation + "!";
+    document.getElementById("resultText").innerText = "Learn to program " + langRecommendation + "!";
     // Update results messaging that provides detail from Wikipedia about lang
     document.getElementById("resultSummary").innerText = langSummaryText;
 
     // Unhide the reset button
-    document.getElementById("resetBtn").removeAttribute("class");
+    document.getElementById("resetBtn").setAttribute("class","btn btn-outline-warning");
     // Event Listener for clicking the reset button
     resetBtn.addEventListener("click", reset);
   });
